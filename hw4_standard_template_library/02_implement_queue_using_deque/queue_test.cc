9f82655ba810bdd5ccf2d4a235f66471618c0045
Marco Wang <m.aesophor@gmail.com>
2019-04-29 09:47:34 +0800

    update queue unit test

--- hw4_standard_template_library/02_implement_queue_using_deque/queue_test.cc
+++ hw4_standard_template_library/02_implement_queue_using_deque/queue_test.cc
@@ -1,24 +1,42 @@
 #include "queue.cc"
 
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
 int main() {
   Queue<int> q;
-  cout << q.to_string() << endl << endl;
+
+  // Test Queue<T>::push_back
+  q.push_back(5);
+  assert(q.to_string() == "5");
+
+  // Test Queue<T>::push_front
+  q.push_front(8);
+  assert(q.to_string() == "8->5");
+
+  // Test Queue<T>::pop_back
+  q.pop_back();
+  assert(q.to_string() == "8");
+
+  // Test Queue<T>::pop_front
+  q.pop_front();
+  assert(q.to_string() == "");
+
+
 
   q.push_back(8);
-  cout << q.to_string() << endl << endl;
+  cout << q.to_string() << endl;
 
   q.push_back(5);
-  cout << q.to_string() << endl << endl;
+  cout << q.to_string() << endl;
   
   q.push_back(2);
-  cout << q.to_string() << endl << endl;
+  cout << q.to_string() << endl;
 
   q.push_back(4);
-  cout << q.to_string() << endl << endl;
+  cout << q.to_string() << endl;
 
   q.push_back(7);
   cout << q.to_string() << endl << endl;
@@ -26,10 +44,14 @@ int main() {
   cout << "Sort queue in incrementing order: " << endl;
   bool incrementing = true;
   q.sort(incrementing);
+  assert(q.to_string() == "2->4->5->7->8");
   cout << q.to_string() << endl << endl;
 
   cout << "Sort queue in decrementing order: " << endl;
   incrementing = false;
   q.sort(incrementing);
+  assert(q.to_string() == "8->7->5->4->2");
   cout << q.to_string() << endl << endl;
+
+  cout << "All test passed" << endl;
 }
